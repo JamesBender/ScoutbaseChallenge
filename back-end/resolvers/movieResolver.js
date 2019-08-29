a83cae113a94663b65695df1173447a8aa6b2d7f
James Bender <James@JamesCBender.com>
2019-08-29 10:09:21 +0100

    Removed debug message indicating if the user was authenticated

--- back-end/resolvers/movieResolver.js
+++ back-end/resolvers/movieResolver.js
@@ -1,10 +1,7 @@
-const chalk = require('chalk');
-
 module.exports = function(movieModel, authenticationService) {
   return {
     getMovies: async ({ token }) => {
       const authenticatedUser = await authenticationService.authenticateUser(token);
-      console.log(chalk.cyanBright(`User authentication: ${authenticatedUser}`));
       return await movieModel.getMovies({ authenticatedUser });
     },
     getMovie: async ({id, token}) => {
