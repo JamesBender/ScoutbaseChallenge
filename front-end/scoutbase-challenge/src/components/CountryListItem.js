48aa083b1f2e086cd24e88492e785022cb08d586
James Bender <James@JamesCBender.com>
2019-08-31 18:03:14 +0100

    Got basic country data and a very rough layout added to the CountryListItem

--- front-end/scoutbase-challenge/src/components/CountryListItem.js
+++ front-end/scoutbase-challenge/src/components/CountryListItem.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { H2, H4, Label } from './common/StyledComponents';
 
 const CountryListItem = ({ country }) => {
   return (
     <div>
-      <div>
-        <span>{country.name}</span> - <span>{country.phone}</span> - {country.code} - {country.native} -
-        {country.continent.code} - {country.continent.name} - {country.currency} - {country.languages.name}-{' '}
-        {country.emoji} - {country.emojiU}
+      <div className="workingDiv">
+        <div className="countryName">
+          <H2>
+            <span>{country.name}</span> - {country.emoji}
+          </H2>
+          <H4>({country.code})</H4>
+        </div>
+        <div className="countryDetails">
+          <Label>Phone:</Label>
+          <span>{country.phone}</span>
+          <br />
+          <Label>Native Spelling:</Label>
+          <span>{country.native}</span>
+          <br />
+        </div>
       </div>
+      <div className="seperate"></div>
       <br />
     </div>
   );
---
+++ front-end/scoutbase-challenge/src/components/common/StyledComponents.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const H2 = styled.h2`
+  margin-top: 0;
+  margin-bottom: 10px;
+`;
+
+export const H4 = styled.h4`
+  margin: 5px;
+`;
+
+export const Label = styled.label`
+  margin-right: 5px;
+`;
